bcf723dfed949cff9f32cf3e62edfd9adceaaae9
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-04-03 10:53:06 +0530

    Create tabulation.cpp

---
+++ 0064-minimum-path-sum/tabulation.cpp
@@ -0,0 +1,29 @@
+```
+  int minSumPath(vector<vector<int>> &grid) {
+
+    // Write your code here.
+    int m =  grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m,vector<int> (n,-1));
+    // writting the tabulation code for the same
+    for(int i = 0;i<m;i++){
+        for(int j = 0;j<n;j++){
+            if(i==0 and j==0) dp[i][j] = grid[i][j];
+            else{
+                int up = grid[i][j];
+                if(i>0) up+=dp[i-1][j];
+                else up+=1e9;
+                
+                int left = grid[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left+=1e9;
+                
+                dp[i][j] = min(up,left);
+            }
+            
+        }
+    }
+
+    return dp[m-1][n-1];
+}
+```
