ce5b811640ba62a773a0af6d4e93ed1605ee5bb7
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-08-14 07:28:23 -0400

    Time: 31 ms (84.22%), Space: 13.3 MB (48.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    vector<int> dir = {-1,0,1,0,-1};
     
-    void dfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>>& grid){
-        vis[row][col]=1;
-        for(int k=0;k<4;k++)
-        {
-            int nrow = row + dir[k];
-            int ncol = col + dir[k+1];
-            if(nrow>=0 and nrow<grid.size() and ncol>=0 and ncol<grid[0].size())
-            {
-                if(grid[nrow][ncol]=='1' and vis[nrow][ncol]==0)
-                {
-                    dfs(nrow,ncol,vis,grid);
-                }
-            }
-        }
+    void dfs(int row, int col,vector<vector<int>> &vis,vector<vector<char>>& grid){
+        if(row<0 or col<0 or row >= grid.size() or col>=grid[0].size() or vis[row][col]==1 or grid[row][col]=='0') return;
+        vis[row][col] = 1; 
+        dfs(row-1,col,vis,grid);
+        dfs(row,col+1,vis,grid);
+        dfs(row+1,col,vis,grid);
+        dfs(row,col-1,vis,grid);
+        
         
     }
+    
     int numIslands(vector<vector<char>>& grid) {
-        
-       
-        int n = grid.size();
-        int m = grid[0].size();
-         vector<vector<int>> vis(grid.size(),vector<int> (grid[0].size(),0));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int> (n,0));
         int ct = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(vis[i][j]==0 and grid[i][j]=='1'){
+        for(int row = 0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(vis[row][col]==0 and grid[row][col] == '1'){
                     ct++;
-                    dfs(i,j,vis,grid);
+                    dfs(row,col,vis,grid);
                 }
             }
         }
         
-        return ct; 
+        return ct;
+        
     }
 };
