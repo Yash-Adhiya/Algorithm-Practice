1bff8f1e097850be359cf0e7aef54284c8a066c0
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-08-24 23:45:13 +0530

    Time: 12 ms (86.06%), Space: 18 MB (13.70%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,16 +4,21 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int ans;
-        set<int> s;
-        for(int i=0;i<n;i++){
-            s.insert(nums[i]);
-        }
-        for(int i=0;i<=n;i++){
-            if(s.find(i)==s.end()){
-                ans = i;
-            }
-        }
+        // set<int> s;
+        // for(int i=0;i<n;i++){
+        //     s.insert(nums[i]);
+        // }
+        // for(int i=0;i<=n;i++){
+        //     if(s.find(i)==s.end()){
+        //         ans = i;
+        //     }
+        // }
         
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        ans = n*(n+1)/2 - sum;
         return ans;
     }
 };
