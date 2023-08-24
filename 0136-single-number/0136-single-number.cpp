99abeefaff3bab6a6189824479f69dea374e3eba
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-08-25 00:10:42 +0530

    Time: 20 ms (53.49%), Space: 16.9 MB (67.11%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans;
-        unordered_map<int,int> mp;
-        for(auto val:nums){
-            mp[val]++;
+         int n = nums.size();
+        // int ans;
+        // unordered_map<int,int> mp;
+        // for(auto val:nums){
+        //     mp[val]++;
+        // }
+        // for(auto it:mp){
+        //     if(it.second==1){
+        //         ans = it.first;
+        //     }
+        // }
+        // return ans;
+        
+        // better  approach using xor property
+        int xorr =0;
+        for(int i =0;i<n;i++){
+            xorr = xorr ^ nums[i];
         }
-        for(auto it:mp){
-            if(it.second==1){
-                ans = it.first;
-            }
-        }
-        return ans;
+        
+        return xorr;
     }
 };
