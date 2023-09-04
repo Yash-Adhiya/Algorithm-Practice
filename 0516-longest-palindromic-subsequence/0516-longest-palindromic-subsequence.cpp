813beef54e4ec290d40b7e5ea90d1bf29b5a5ee7
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-09-04 23:45:33 +0530

    Time: 100 ms (71.42%), Space: 6.7 MB (92.66%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -6,25 +6,30 @@ public:
         
         int n = t.size();
         
-        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-            dp[0][i] = 0;
-        }
+        // vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        // space optimization 
+        
+        vector<int> prev(n+1,0),curr(n+1,0);
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 0;
+        //     dp[0][i] = 0;
+        // }
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(t[i-1]==s[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
                 }
             }
             
+            prev = curr;
+            
         }
         
-        return dp[n][n];
+        return prev[n];
         
     }
 };
