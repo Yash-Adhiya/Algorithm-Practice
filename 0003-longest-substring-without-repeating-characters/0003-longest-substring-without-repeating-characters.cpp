e7dc0855988a6496fd5ac74a61440b20b56064f3
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-09-05 20:44:42 +0530

    Time: 3 ms (97.30%), Space: 8.3 MB (68.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,20 +5,17 @@ public:
             return 0;
         }
         int n = s.size();
-        set<int> st;
+        vector<int> mpp(256,-1);
         int l = 0;
         int r =0;
         int ans = INT_MIN;
-        for(int r=0;r<n;r++){
-            if(st.find(s[r])!=st.end()){
-                while(l<r && st.find(s[r])!=st.end()){
-                    st.erase(s[l]);
-                    l++;
-                }
+        while(r<n){
+            if(mpp[s[r]]!=-1){
+                l = max(l, mpp[s[r]]+1);
             }
-            
-            st.insert(s[r]);
-            ans = max(ans, r-l+1);
+            mpp[s[r]] = r;
+            r++;
+            ans = max(ans, r-l);
         }
         
         
