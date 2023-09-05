e46f12d84ce469b32ac761c8b66dbf79fd521b09
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-09-05 16:00:12 +0530

    Time: 8 ms (95.33%), Space: 6.7 MB (98.12%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -4,24 +4,27 @@ public:
         int n = word1.size();
         int m = word2.size();
         
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int j=0;j<=m;j++){
-            dp[0][j] = 0;
-        }
+//         vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+//         for(int i=0;i<=n;i++){
+//             dp[i][0] = 0;
+//         }
+//         for(int j=0;j<=m;j++){
+//             dp[0][j] = 0;
+//         }
         
+        
+        vector<int> prev(m+1,0),curr(m+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                else curr[j] = max(prev[j],curr[j-1]);
             }
+            prev = curr;
         }
         
-        int k = dp[n][m];
+        int k = prev[m];
         
         return (n-k) + (m-k);
     }
