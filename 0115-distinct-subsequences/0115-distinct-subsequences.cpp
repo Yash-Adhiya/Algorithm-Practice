1ca0a7108be10cd4b65f2dfbc330e913337c1c4a
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2023-09-05 18:18:06 +0530

    Time: 39 ms (57.44%), Space: 18.6 MB (55.50%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
-    const int prime =1e9+7;
-    int countUtil(string s1, string s2, int ind1, int ind2,vector<vector<int>>& dp){
-    if(ind2<0)
-        return 1;
-    if(ind1<0)
-        return 0;
-    
-    if(dp[ind1][ind2]!=-1)
-        return dp[ind1][ind2];
-    
-    if(s1[ind1]==s2[ind2]){
-        int leaveOne = countUtil(s1,s2,ind1-1,ind2-1,dp);
-        int stay = countUtil(s1,s2,ind1-1,ind2,dp);
+    int prime = 1e9 + 7;
+    int f(int i,int j,string s, string t, vector<vector<int>> dp){
+        // base condition 
+       
+        if(j<0) return 1;
+         if(i<0) return 0;
         
-        return dp[ind1][ind2] = (leaveOne + stay)%prime;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==t[j]){
+            int leaves = f(i-1,j-1,s,t,dp);
+            int stays = f(i-1,j,s,t,dp);
+            return dp[i][j] = (leaves+stays)%prime;
+        }
+        else{
+            return dp[i][j] = f(i-1,j,s,t,dp)%prime;
+        }
+        
+        
+    }
+    int numDistinct(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    
+    for(int i=0;i<n+1;i++){
+        dp[i][0]=1;
+    }
+    for(int i=1;i<m+1;i++){
+        dp[0][i]=0;
     }
     
-    else{
-        return dp[ind1][ind2] = countUtil(s1,s2,ind1-1,ind2,dp);
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            
+            if(s1[i-1]==s2[j-1])
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+            else
+                dp[i][j] = dp[i-1][j];
+        }
     }
-}
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return countUtil(s,t,n-1,m-1,dp);
+    
+    
+    return dp[n][m];
     }
 };
